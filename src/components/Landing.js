5fd1921687e8ac2a50ba649896611dee9f883a1c
Pakzad <96146741+pakzadmusic@users.noreply.github.com>
2022-07-30 15:44:08 +0430

    prettier added

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -8,50 +8,53 @@ import Loader from './Loader';
 import Coin from './Coin';
 
 // Styles
-import styles from "./Landing.module.css";
+import styles from './Landing.module.css';
 
 const Landing = () => {
 
     const [coins, setCoins] = useState([]);
-    const [search, setSearch] = useState("");
-
+    const [search, setSearch] = useState('');
+    
     useEffect(() => {
         const fetchAPI = async () => {
             const data = await getCoin();
-            console.log(data)
-            setCoins(data)
-        }
+            setCoins(data);
+        };
 
-        fetchAPI()
-    }, [])
+        fetchAPI();
+    }, []);
 
-    const searchHandler = event => {
-        setSearch(event.target.value)
-    }
+    const searchHandler = (event) => {
+        setSearch(event.target.value);
+    };
 
-    const searchedCoins = coins.filter(coin => coin.name.toLowerCase().includes(search.toLowerCase()))
+    const searchedCoins = coins.filter((coin) => coin.name.toLowerCase().includes(search.toLowerCase()));
 
     return (
         <>
-            <input className={styles.input} type="text" placeholder="Search" value={search} onChange={searchHandler} />
-            {
-                coins.length ?
-                    <div className={styles.coinContainer}>
-                        {
-                            searchedCoins.map(coin => <Coin
-                                key={coin.id}
-                                name={coin.name}
-                                image={coin.image}
-                                symbol={coin.symbol}
-                                price={coin.current_price}
-                                marketCap={coin.market_cap}
-                                priceChange={coin.price_change_percentage_24h}
-                            />)
-                        }
-                    </div> :
+            <input
+                className={styles.input}
+                type="text"
+                placeholder="Search"
+                value={search}
+                onChange={searchHandler}
+            />
 
-                    <Loader />
-            }
+            {coins.length ? (
+                <div className={styles.coinContainer}>
+                    {searchedCoins.map((coin) => (
+                        <Coin
+                            key={coin.id}
+                            name={coin.name}
+                            image={coin.image}
+                            symbol={coin.symbol}
+                            price={coin.current_price}
+                            marketCap={coin.market_cap}
+                            priceChange={coin.price_change_percentage_24h}
+                        />
+                    ))}
+                </div>
+            ) : (<Loader />)}
         </>
     );
 };
